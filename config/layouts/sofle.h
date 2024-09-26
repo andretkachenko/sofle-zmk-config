8584bdcde28df52a6923643e96ef42192409d997
Andre Tkachenko <andre.tkachenko@outlook.com>
2024-09-26 15:17:54 +0300

    Split keymap into modules (#4)
    * split devicetree instructions into modules
    
    this is a test run, to ensure that I properly understood how I can
    split my keymap file to reuse them in multiple layout.
    I want to consolidate different layouts that I now keep as separate
    branches into a single branch, which will produce zip with multiple
    layout options for me to chose.
    
    * fix includes
    
    * fix paths to includes
    
    * fix include declaration
    
    * fix includes
    
    * fix include path
    
    * add behaviors include into layer files
    
    * rename folder to avoid ambiguity
    
    * return unified keymap
    
    * include sofle.dtsi
    
    * include layers dtsi-s
    
    * return layers and include behaviors and combos
    
    * return behaviors
    
    * add zmk includes to the combos
    
    * return combos
    
    * add root braces to combos.dtsi
    
    * include combos
    
    * move combos include out of root brackets
    
    * add layer and behavior includes
    
    * fix invalid opening slash
    
    * reorganize files
    
    * move current config to boards
    
    this is a step before unifying all my layouts into one build.
    if this one works as expected, it'll be merged into the corne branch,
    which then will be merged (immediately or through an intermediate
    branch) into the main
    
    * fix build.yaml
    
    In the ZMK Discord server, Saixos has pointed me towards the zmk#1931
    issue, with the answer which would allow me to keep multiple keymaps.
    This is the implementation from that answer.
    
    * fix paths to modules
    
    * rearrange config
    
    People recommend to treat includes that require to jump up rhe folder
    as a code smell. To avoid that, I moved conf and keymap files backs to
    the config folder.
    
    * return to default config location
    
    * revert build changes
    
    * return layout specific args
    
    * add debug layout
    
    * unify conf and remove build comments

---
+++ config/layouts/sofle.h
@@ -0,0 +1,15 @@
+// left-hand keys
+// leftmost column (0, 12, 24, 36) skipped as they are used for modificators (esc, tab, shift)
+#define KEYS_L 1  2  3  4  5 \
+               13 14 15 16 17 \
+               25 26 27 28 29 \
+               37 38 39 40 41
+
+// right-hand keys
+#define KEYS_R 6  7  8  9  10 11 \
+               18 19 20 21 22 23 \
+               30 31 32 33 34 35 \
+               44 45 46 47 48 49 // indexes 42 and 43 missing because they are reserved for encoders
+
+// thumb keys
+#define THUMBS 50 51 52 53 54 55 56 57 58 59
