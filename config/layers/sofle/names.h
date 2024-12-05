4f4bff474df9298e61e22d562bf04f25bee35b37
Andre Tkachenko <andre.tkachenko@outlook.com>
2024-12-05 02:52:17 +0200

    move sym and hub layers to the top of the stack

--- config/layers/sofle/names.h
+++ config/layers/sofle/names.h
@@ -1,8 +1,8 @@
 #define BASE 0
 #define NAV  1
 #define NUM  2
-#define SYM  3
-#define HUB  4
-#define SYS  5
-#define UNI  6
-#define FN   7
+#define SYS  3
+#define UNI  4
+#define FN   5
+#define SYM  6
+#define HUB  7
