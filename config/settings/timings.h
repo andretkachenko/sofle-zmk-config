b4bcc314aa7fa3c7bc86090a456f688fde971cbf
Andre Tkachenko <andre.tkachenko@outlook.com>
2024-12-10 13:52:51 +0200

    speed up mouse pointer and scroll

--- config/settings/timings.h
+++ config/settings/timings.h
@@ -1,3 +1,7 @@
 #define TAP_TIME_MS 250   
 #define QUICK_TAP_MS 175                              
 #define IDLE_MS 130
+
+// override default mouse speed, defaults in comments
+#define ZMK_MOUSE_DEFAULT_MOVE_VAL 1500  // 600
+#define ZMK_MOUSE_DEFAULT_SCRL_VAL 20    // 10
