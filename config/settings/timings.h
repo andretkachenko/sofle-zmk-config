11ebad9f17db00d6bf9f39ac478af55f091da457
Andre Tkachenko <andre.tkachenko@outlook.com>
2024-12-14 20:10:02 +0200

    move pointer velocity override
    test if moving defines will make them work

--- config/settings/timings.h
+++ config/settings/timings.h
@@ -1,7 +1,3 @@
 #define TAP_TIME_MS 250   
 #define QUICK_TAP_MS 175                              
 #define IDLE_MS 130
-
-// override default mouse speed, defaults in comments
-#define ZMK_MOUSE_DEFAULT_MOVE_VAL 300000  // 600
-#define ZMK_MOUSE_DEFAULT_SCRL_VAL 20    // 10
